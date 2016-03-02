b0c2fbc2dd58592b0364a0fe6abffb322704f430
Minh-Quan TRAN <qtran@ijenko.com>
2016-03-02 10:45:03 +0100

    comment in README

--- dev-varnish/puppet/all/modules/base/manifests/init.pp
+++ dev-varnish/puppet/all/modules/base/manifests/init.pp
@@ -6,17 +6,19 @@ class base (
   if ($php_version == 5.5) or ($php_version == 5.6) or ($php_version == 7.0) {
 
 	  # add dotdeb repo to sources.list.d
-	  file { 'dotdeb':
-	    path     => '/etc/apt/sources.list.d/dotdeb.list',
+	  file { '/etc/apt/sources.list.d/dotdeb.list':
 	    ensure   => 'present',
 	    source   => "puppet:///modules/base/dotdeb.list",
-	  }
+			before   => Exec['dotdeb_add_key'],
+		}
 
-	  exec { 'update':
-      onlyif => '/usr/bin/test ! -f /etc/apt/sources.list.d/dotdeb.list',
-	    command => '/usr/bin/wget http://www.dotdeb.org/dotdeb.gpg && sudo apt-key add dotdeb.gpg && /usr/bin/apt-get update',
-	    require => File["dotdeb"],
-	  }
+		exec { 'dotdeb_add_key':
+			unless  => 'apt-key list | grep dotdeb.org',
+			command => "curl http://www.dotdeb.org/dotdeb.gpg | apt-key add - && apt-get update",
+			path    => ["/usr/bin", "/usr/sbin", "/bin"],
+			require => Package['curl'],
+		}
+
   }
 
   package { 'curl':
--- dev-varnish/puppet/all/modules/varnish/manifests/install.pp
+++ dev-varnish/puppet/all/modules/varnish/manifests/install.pp
@@ -1,32 +1,24 @@
 class varnish::install {
 
-  file { 'debian_package':
-    path     => '/etc/apt/sources.list.d/varnish.list',
+  file { '/etc/apt/sources.list.d/varnish.list':
     ensure   => 'present',
     source   => "puppet:///modules/varnish/varnish.list",
-    before   => Exec['add_key'],
+    before   => Exec['varnish_add_key'],
   }
 
-  exec { 'add_key':
+  exec { 'varnish_add_key':
     unless  => 'apt-key list | grep varnish-cache.org',
-    command => "curl https://repo.varnish-cache.org/debian/GPG-key.txt | apt-key add -",
+    command => "curl https://repo.varnish-cache.org/debian/GPG-key.txt | apt-key add - && apt-get update",
     path    => ["/usr/bin", "/usr/sbin", "/bin"],
-    before  => Exec['apt-get_update'],
-    require => Package['curl'],
+    require => Package['curl', 'apt-transport-https'],
   }
 
-  exec { 'apt-get_update':
-    command => "/usr/bin/apt-get update",
-    require => Exec['add_key'],
-  }
-
-  package { 'apt-transport-https':
+  package { ['apt-transport-https']:
     ensure  => installed,
-    before  => Exec['apt-get_update'],
   }
 
-  package { 'varnish':
+  package { ['varnish']:
     ensure  => installed,
-    require => Exec['apt-get_update'],
   }
+
 }
